4b2b0588a7d28fec07e8ed2d3b7da2e1afbfa20a
Wiktoria <wpawlik73@gmail.com>
2017-05-31 14:40:22 +0200

    test

--- scripts.js
+++ scripts.js
@@ -1,1 +1,20 @@
 // scripts.js
+
+var womenNames = ['Ania', 'Asia', 'Zofia'];
+var menNames = ['Adam', 'Marian', 'Stefan'];
+
+// łączenie tablic
+var allNames = womenNames.concat(menNames);
+console.log(allNames);
+
+//dodanie imienia
+//allNames.splice(1, 0, 'Tadek');
+//console.log(allNames);
+
+//spr. powtórzeń
+
+var newName = 'Ania';
+  if (allNames.indexOf(newName) === -1) {
+    var x = allNames.push(newName);
+  }
+console.log(allNames);

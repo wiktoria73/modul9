7763b6ca7594f4880f1bde9e26ecac6e466493d2
Wiktoria <wpawlik73@gmail.com>
2017-06-01 21:21:15 +0200

    test

--- scripts.js
+++ scripts.js
@@ -1,1 +1,13 @@
 // scripts.js
+
+function drawTree(i) {
+  for (i = 1; i < 6; i++) {
+    var star = '';
+
+    for (j = i ; j < 6; j++) {
+      star += '*';
+    }
+    console.log(star);
+  }
+}
+drawTree();

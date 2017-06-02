0e32b59054c6b7f13e4aebd72f0bec6d65c9067b
Wiktoria <wpawlik73@gmail.com>
2017-06-03 00:52:36 +0200

    test2

--- scripts.js
+++ scripts.js
@@ -1,24 +1,11 @@
 // scripts.js
 
-//wszystkie elemnety 'button'
+//wszystkie elementy 'button'
 var getButton = document.getElementsByClassName('button');
 console.log(getButton);
 
-//ilośc obiektów w tablicy
-var amount = getButton.length;
-console.log(amount);
 
 //pętla
-for (i=0 ; i<amount ; i++) {
-
+for (var i=0 ; i<getButton.length ; i++) {
+    alert(getButton[i].innerText);
     }
-
-
-//console.log(i+1);
-
-//próba
-var guzik = document.getElementById('button2').innerText;
-console.log(guzik);
-
-var guzik2 = document.getElementsByClassName('button').innerText;
-console.log(guzik2);

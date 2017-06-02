61c2e60372cc30313923f21b3578c9e5b9f7428c
Wiktoria <wpawlik73@gmail.com>
2017-06-02 23:32:37 +0200

    test

--- scripts.js
+++ scripts.js
@@ -1,1 +1,15 @@
 // scripts.js
+
+var buttonElem = document.getElementById('addElem')
+var list = document.getElementById('list')
+
+buttonElem.addEventListener('click', function(e) {
+  var element = document.createElement('li');
+  //console.log(element);
+  
+  var level = document.getElementsByTagName('li');
+  //console.log(level.length)
+
+  element.innerHTML = 'item ' + level.length;
+  list.appendChild(element);
+})
